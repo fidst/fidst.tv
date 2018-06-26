bf02cb148018726a7ff23ab79350a8960073d110
Isaac K-S <isaacklassenselberg@gmail.com>
2018-06-25 22:15:55 -0400

    removed steps to success on main page

--- yto/index.php
+++ yto/index.php
@@ -58,6 +58,28 @@ include 'header.html';
 			<a href="#who" class="more scrolly">Gander</a>
 		</section>
 
+		<!-- who -->
+		<section id="who" class="wrapper style1 special">
+			<div class="inner">
+				<header class="major">
+					<h2>Who is Isaac?</h2>
+					<p>A bright-eyed, doesn't look back, excited, sorta human.
+						<br> Isaac is a graduate of Fanshawe College, London, Canada in computer technology. With experience in community management, technical support, and corporate technology solutions.
+						<br> </p>
+				</header>
+				<ul class="icons major">
+					<li data-tooltip="Computer Mindset" data-tooltip-position="left"><span class="icon fa-desktop major style2"><span class="label">Computer Mindset</span></span>
+					</li>
+					<li data-tooltip="Leadership" data-tooltip-position="left"><span class="icon fa-power-off major style2"><span class="label">Leadership</span></span>
+					</li>
+					<li data-tooltip="Community" data-tooltip-position="left"><span class="icon fa-heart-o major style2"><span class="label">Community</span></span>
+					</li>
+					<li data-tooltip="Problem Solving" data-tooltip-position="left"><span class="icon fa-code major style2"><span class="label">Problem Solving</span></span>
+					</li>
+				</ul>
+				<p>"Life is full of speed-bumps and stepping stones. Cheers to those who help along the way."</p>
+			</div>
+		</section>
 
 <!-- Gallery -->
 <?php
